7c857c6c3f58b326c252f6be07f178896d388e2b
otavioabreu27 <otavio.abreu96@gmail.com>
2023-07-06 13:35:19 -0300

    black formatting

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,10 +1,11 @@
 import pytest
 from ..app import main
 
+
 class TestClass:
-  def test_hello_world(self):
-    """
-      Hello test
-    """
-    
-    assert main.hello_world() == {"Hello": "World"}  # it should return hello world 
+    def test_hello_world(self):
+        """
+        Hello test
+        """
+
+        assert main.hello_world() == {"Hello": "World"}  # it should return hello world
